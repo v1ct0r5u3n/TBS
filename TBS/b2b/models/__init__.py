ce23afa35fa42ac5ffb70dba177bc33bd25e202a
v1ct0r5u3n <301464@qq.com>
2022-01-17 13:02:42 +0800

    clean b2b models

--- TBS/b2b/models/__init__.py
+++
@@ -1,8 +0,0 @@
-'''
-from .stock import Stock
-from .transfer import Transfer,TransferConfirm
-from .lend import Lend,LendReturn
-from .remake import Assemble,Disassemble
-from .repair import Repair,RepairReturn
-'''
-from .base import B2BRecord,B2BConfirm
